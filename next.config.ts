7fd7613cee01d075dece8ae954c69caa6a2666d5
Calix <cx.cxszc@gmail.com>
2026-06-08 16:11:20 -0400

    allow imgbb image domain

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
